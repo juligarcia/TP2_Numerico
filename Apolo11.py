72cf7ce7a025df2b092c2523e54b5c17fe6d776a
juligarcia <juli.garcia.77@gmail.com>
2018-11-25 14:44:00 -0300

    Runge kutta 2 listo, esta calculando algo mal, habria que chequear, pero el codigo esta todo

--- Apolo11.py
+++ Apolo11.py
@@ -68,19 +68,35 @@ def condicionesIniciales(V_dada):
 
 	return condiciones_0
 
-def X_n(h, condicionesActuales, counter):
+def X_n(h, condicionesActuales, counter, RK = False):
 
 	"""Calcula una iteracion de la ecuacion Xn+1"""
 
-	condicionesActuales[counter]['Xn'] = (condicionesActuales[counter].get('Xn') + h * condicionesActuales[counter].get('Vxn'))
+	X = (condicionesActuales[counter].get('Xn') + h * condicionesActuales[counter].get('Vxn'))
 
-def  Y_n(h, condicionesActuales, counter):
+	if RK is False:
+
+		condicionesActuales[counter]['Xn'] = X
+
+	else:
+
+		return X
+
+def  Y_n(h, condicionesActuales, counter, RK = False):
 
 	"""Calcula una iteracion de la ecuacion Yn+1"""
 
-	condicionesActuales[counter]['Yn'] = (condicionesActuales[counter].get('Yn') + h * condicionesActuales[counter].get('Vyn'))
+	Y = (condicionesActuales[counter].get('Yn') + h * condicionesActuales[counter].get('Vyn'))
 
-def Vy_n(h, condicionesActuales, counter, M2, w, G = 6.674 * (10 ** -11), M1 = 5972 * (10 ** 21), y1 = 0, x1 = -4.670 * (10 ** 6), y2 = 0, x2 = 379.7 * (10 ** 6)):
+	if RK is False:
+
+		condicionesActuales[counter]['Yn'] = Y
+
+	else:
+		
+		return Y
+
+def Vy_n(h, condicionesActuales, counter, M2, w, RK = False, G = 6.674 * (10 ** -11), M1 = 5972 * (10 ** 21), y1 = 0, x1 = -4.670 * (10 ** 6), y2 = 0, x2 = 379.7 * (10 ** 6)):
 
 	"""Calcula una iteracion de la ecuacion Vx n+1"""
 
@@ -114,9 +130,17 @@ def Vy_n(h, condicionesActuales, counter, M2, w, G = 6.674 * (10 ** -11), M1 = 5
 
 		fuerzaCentripeta = (w ** 2) * dg * math.sin(ac)
 
-	condicionesActuales[counter]['Vyn'] = (condicionesActuales[counter].get('Vyn') + h * (fuerzaTerrea + fuerzaLunar + fuerzaCentripeta))
+	Vy = (condicionesActuales[counter].get('Vyn') + h * (fuerzaTerrea + fuerzaLunar + fuerzaCentripeta))
 
-def Vx_n(h, condicionesActuales, counter, M2, w, G = 6.674 * (10 ** -11), M1 = 5972 * (10 ** 21), y1 = 0, x1 = -4.670 * (10 ** 6), y2 = 0, x2 = 379.7 * (10 ** 6)):
+	if RK is False:
+
+		condicionesActuales[counter]['Vyn'] = Vy
+
+	else:
+
+		return Vy
+
+def Vx_n(h, condicionesActuales, counter, M2, w, RK = False, G = 6.674 * (10 ** -11), M1 = 5972 * (10 ** 21), y1 = 0, x1 = -4.670 * (10 ** 6), y2 = 0, x2 = 379.7 * (10 ** 6)):
 
 	"""Calcula una iteracion de la ecuacion Vx n+1"""
 
@@ -150,7 +174,16 @@ def Vx_n(h, condicionesActuales, counter, M2, w, G = 6.674 * (10 ** -11), M1 = 5
 
 		fuerzaCentripeta = (w ** 2) * dg * math.cos(ac)
 
-	condicionesActuales[counter]['Vxn'] = (condicionesActuales[counter].get('Vxn') + h * (fuerzaTerrea + fuerzaLunar + fuerzaCentripeta))
+	Vx = (condicionesActuales[counter].get('Vxn') + h * (fuerzaTerrea + fuerzaLunar + fuerzaCentripeta))
+
+	if RK is False:	
+
+		condicionesActuales[counter]['Vxn'] = Vx
+
+	else:
+
+		return Vx
+
 
 def dSquared(condicion, estaTierra = False, estaLuna = False, y1 = 0, x1 = -4.670 * (10 ** 6), y2 = 0, x2 = 379.7 * (10 ** 6)):
 
@@ -215,14 +248,21 @@ def generarTrayectoria(condicionesActuales):
 
 	return [Tx, Ty]
 
-def iterar(h, condicionesActuales, counter, M2, w):
+def iterarEuler(h, condicionesActuales, counter, M2, w):
 
-	"""Funcion para iterar"""
+	"""Funcion para iterar con Euler"""
 
 	Vx_n(h, condicionesActuales, counter, M2, w)
 	Vy_n(h, condicionesActuales, counter, M2, w)
 	X_n(h, condicionesActuales, counter)
 	Y_n(h, condicionesActuales, counter)
+
+def iterarRK(h, condicionesActuales, counter, M2, w):
+
+	"""Funcion para iterar con RK"""
+
+	RK2_velocidad(condicionesActuales, counter, h, M2, w)
+	RK2_posicion(condicionesActuales, counter, h)
 
 def plotTrayectoria(trayectoria, estaLuna = False):
 
@@ -273,7 +313,7 @@ def eulerExplicito(condicionesActuales, v0, M2 = 73.48 * (10 ** 21), w = 4.236 *
 
 	counter += 1
 
-	iterar(h, condicionesActuales, counter, M2, w)
+	iterarEuler(h, condicionesActuales, counter, M2, w)
 
 	while rotation != True & isOnReach :
 
@@ -286,7 +326,7 @@ def eulerExplicito(condicionesActuales, v0, M2 = 73.48 * (10 ** 21), w = 4.236 *
 
 		counter += 1
 		
-		iterar(h, condicionesActuales, counter, M2, w)
+		iterarEuler(h, condicionesActuales, counter, M2, w)
 
 		if areWeDeadYet(condicionesActuales[counter]):
 			print('>>>>>>>>La nave se estrello!<<<<<<<<\n')
@@ -423,16 +463,110 @@ def rocket():
 	print('      \\/\\/\\/\\/')
 	print('    \\/\\/\\/\\/\\/\\/')
 	print('  \\/\\/\\/\\/\\/\\/\\/\\/')
+	print('\n>Gracias por volar con nosotros!\n>>Esperamos que no se haya estrellado\n')
 
-def rungeKuta2(condicionesActuales, v0, M2 = 73.48 * (10 ** 21), w = 4.236 * (10 ** -7)):
+def rungeKutta2(condicionesActuales, v0, M2 = 73.48 * (10 ** 21), w = 4.236 * (10 ** -7)):
 
-	"""Resolucion mediante Runge Kuta de orden 2"""
+	"""Resolucion mediante Runge Kutta de orden 2"""
 
-	#terminar
+	#Se inicializan las condiciones actuales en las iniciales y se crea el diccionario que luego se implementara
 
-def main():
+	condicionesActuales.append(condicionesIniciales(V_dada = v0))
 
-	printIntegrantes()
+	counter = 0
+	rotation = False
+	isOnReach = True
+	R = calcularR()
+
+	#Se chequea si la luna esta presente mediante el valor de su masa M2
+
+	h = float(input('>>Ingrese un valor de paso: '))
+	print('\n')
+
+	if h <= 0:
+		print('>>>Paso Incorrecto')
+		return False
+
+	while rotation != True & isOnReach :
+
+		#Se toma una 'vuelta' de referencia para frenar las iteraciones o el tiempo en que tarda en dar 
+		#600 vueltas a la tierra (Es un timepo limite por si se dispara el satelite)
+
+		aux = condicionesActuales[counter].copy()
+
+		condicionesActuales.append(aux)
+
+		counter += 1
+		
+		iterarRK(h, condicionesActuales, counter, M2, w)
+
+		if areWeDeadYet(condicionesActuales[counter]):
+			print('>>>>>>>>La nave se estrello!<<<<<<<<\n')
+			break
+
+		if fullRotationCheck(condicionesActuales[counter]):
+			print('>>>>>>>>Orbita completada!<<<<<<<<\n')
+			rotation = True
+
+		if onReach(condicionesActuales[counter]) is False:
+			print('>>>>>>>>Fuera de alcance<<<<<<<<')
+			print('>>>>>>>>Perdimos la nave<<<<<<<<')
+			print('>>>>>>>>Notifique a los familiares de los tripulantes<<<<<<<<\n')
+			isOnReach = False
+		
+	printBitacora(condicionesActuales[0], h, counter)
+
+	trayectoria = generarTrayectoria(condicionesActuales)
+
+	if M2 is 0:
+		plotTrayectoria(trayectoria)
+
+	else:
+		plotTrayectoria(trayectoria, estaLuna = True)
+
+	return True
+
+
+
+def RK2_velocidad(condicionesActuales, counter, h, M2, w):
+
+	q1 = h * Vx_n(h, condicionesActuales, counter, M2, w, RK = True)
+
+	condicionesActuales[counter]['Vxn'] = condicionesActuales[counter].get('Vxn') + q1
+
+	p1 = h * Vy_n(h, condicionesActuales, counter, M2, w, RK = True)
+
+	condicionesActuales[counter]['Vyn'] = condicionesActuales[counter].get('Vyn') + p1
+
+	q2 = h * Vx_n(h, condicionesActuales, counter, M2, w, RK = True)
+	p2 = h * Vy_n(h, condicionesActuales, counter, M2, w, RK = True)
+
+	condicionesActuales[counter]['Vxn'] = condicionesActuales[counter].get('Vxn') - q1
+	condicionesActuales[counter]['Vyn'] = condicionesActuales[counter].get('Vyn') - p1
+
+	condicionesActuales[counter]['Vxn'] = condicionesActuales[counter].get('Vxn') + 0.5 * (q1 + q2)
+	condicionesActuales[counter]['Vyn'] = condicionesActuales[counter].get('Vyn') + 0.5 * (p1 + p2)
+
+def RK2_posicion(condicionesActuales, counter, h):
+
+	q1 = h * X_n(h, condicionesActuales, counter, RK = True)
+
+	condicionesActuales[counter]['Xn'] = condicionesActuales[counter].get('Xn') + q1
+
+	p1 = h * X_n(h, condicionesActuales, counter, RK = True)
+
+	condicionesActuales[counter]['Yn'] += condicionesActuales[counter].get('Yn') + p1
+
+	q2 = h * X_n(h, condicionesActuales, counter, RK = True)
+	p2 = h * Y_n(h, condicionesActuales, counter, RK = True)
+
+	condicionesActuales[counter]['Xn'] = condicionesActuales[counter].get('Xn') - q1
+	condicionesActuales[counter]['Yn'] = condicionesActuales[counter].get('Yn') - p1
+
+	condicionesActuales[counter]['Xn'] = condicionesActuales[counter].get('Xn') + 0.5 * (q1 + q2)
+	condicionesActuales[counter]['Yn'] = condicionesActuales[counter].get('Yn') + 0.5 * (p1 + p2)
+
+def opcionEuler():
 
 	opcion = int(input('>Ingrese 1 para probar la orbita terrestre \n>Ingrese 2 para probar la orbita terrestre - lunar\n\n>Opcion: '))
 
@@ -480,11 +614,75 @@ def main():
 				v0 = float(input('>Ingrese una velocidad inicial para el problema, 0 para finalizar.\n>Opcion: '))
 	
 		print('>Se termino la simulacion!')
+
+def opcionRK():
+
+	opcion = int(input('>Ingrese 1 para probar la orbita terrestre \n>Ingrese 2 para probar la orbita terrestre - lunar\n\n>Opcion: '))
+
+	if opcion is 1:
+
+		condicionesActuales = []
+
+		#Se indica, con v0 = 0, que se debe calcular la velocidad inicial
+
+		v0 = 0 
+
+		print('\nSIMULACION DE ORBITA TERRESTRE\n')
+
+		if rungeKutta2(condicionesActuales, v0, M2 = 0, w = 0):
+
+			energiaTotal(condicionesActuales)
+
+			print('>Se termino la simulacion!')
+			rocket()
+
+		else:
+			print('>Fracaso en la simulacion.')
+
+	elif opcion is 2:
+
+		print('\nSIMULACION DE ORBITA TERRESTRE - LUNAR.\n')
+
+		v0 = float(input('>Ingrese una velocidad inicial para el problema, 0 para finalizar.\n\n>Opcion: '))
+
+		if v0 != 0: 
+
+			while v0 != 0:
+
+				condicionesActuales = []
+
+				if rungeKutta2(condicionesActuales, v0):
+
+					energiaTotal(condicionesActuales, estaLuna = True)
+
+					print('>Se termino la simulacion!')
+
+				else:
+					print('>Fracaso en la simulacion')
+
+				v0 = float(input('>Ingrese una velocidad inicial para el problema, 0 para finalizar.\n>Opcion: '))
+	
+		print('>Se termino la simulacion!')
+
+def main():
+
+	printIntegrantes()
+
+	opcion = int(input('>Ingrese 1 para simular con Euler explicito \n>Ingrese 2 para simular con Runge Kutta de orden 2\n\n>Opcion: '))
+
+	if opcion is 1:
+
+		opcionEuler()
+		rocket()
+
+	elif opcion is 2:
+
+		opcionRK()
 		rocket()
 
 	else:
 
-		print('>Opcion incorrecta')
+		print('Opcion incorrecta')
 
 main()
 
